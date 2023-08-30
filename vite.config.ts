10912347032f02e87c6754bb109510e1873a429f
kunish <kunish.butt@gmail.com>
2023-08-30 10:18:11 +0800

    fix: support child path to fix router issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: './',
   build: { chunkSizeWarningLimit: 1000 },
   resolve: { alias: { '~': '/src' } },
   plugins: [devtools(), solidPlugin(), splitVendorChunkPlugin()],
