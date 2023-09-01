a8c9c218985cd3cc470589aa3699a47bf280fd5e
kunish <kunish.butt@gmail.com>
2023-09-01 17:08:00 +0800

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   base: './',
   build: { chunkSizeWarningLimit: 1000 },
   resolve: { alias: { '~': '/src' } },
-  plugins: [solidPlugin(), splitVendorChunkPlugin()],
+  plugins: [
+    solidPlugin(),
+    splitVendorChunkPlugin(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
 })
