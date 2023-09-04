9840141bce39188094db12373d8c358da771ffd1
kunish <kunish.butt@gmail.com>
2023-09-05 02:12:10 +0800

    feat: add safe area paddings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
 import daisyui from 'daisyui'
 import { Config } from 'tailwindcss'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import safeArea from 'tailwindcss-safe-area'
 
 export default {
   content: ['./src/**/*.{css,tsx}'],
-  plugins: [daisyui],
+  plugins: [daisyui, safeArea],
   daisyui: { themes: true },
 } as Config
