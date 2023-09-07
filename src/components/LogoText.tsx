8b5666afbe1602fd0cb2bf0e3416b078bd8a5bef
nb5p <nb5p@users.noreply.github.com>
2023-09-07 21:23:37 +0800

    feat(logo): sexy halloween logo

--- src/components/LogoText.tsx
+++ src/components/LogoText.tsx
@@ -1,14 +1,29 @@
+import { Show } from 'solid-js'
+import { curTheme } from '~/signals'
+
 export const LogoText = () => (
   <a
-    class="text-md flex items-center gap-1 whitespace-nowrap font-bold uppercase sm:text-xl"
+    class="text-md flex items-center gap-1 whitespace-nowrap font-bold sm:text-xl"
     href="https://github.com/metacubex/metacubexd"
     target="_blank"
   >
-    <span class="bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent">
-      metacube
-    </span>
-    <span>(</span>
-    <div class="text-primary transition-transform hover:rotate-90 hover:scale-125">
+    <Show
+      when={curTheme() === 'halloween'}
+      fallback={
+        <span class="bg-gradient-to-br from-primary to-secondary bg-clip-text uppercase text-transparent">
+          metacube
+        </span>
+      }
+    >
+      <>
+        <span class="px-[0.1em] capitalize text-white">meta</span>
+        <span class="rounded-[0.1em] bg-[#f7971d] px-[0.1em] text-black">
+          cube
+        </span>
+      </>
+    </Show>
+    <span class="px-[0.1em]">(</span>
+    <div class="uppercase text-primary transition-transform hover:rotate-90 hover:scale-125">
       xd
     </div>
     <span>)</span>
