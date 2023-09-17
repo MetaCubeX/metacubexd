477fb2233a56e5d3a015a27292580eac0267b7d8
kunish <kunish.butt@gmail.com>
2023-09-17 12:38:53 +0800

    fix(logs): inconsistent gap between search input and table

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -128,7 +128,7 @@ export default () => {
   })
 
   return (
-    <div class="flex h-full flex-col gap-4 p-1">
+    <div class="flex h-full flex-col gap-2 p-1">
       <div class="join w-full">
         <input
           type="search"
