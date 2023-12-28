7ac3085a4b1162569d40d43b4ce20126f118ec46
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2023-12-28 20:01:05 +0700

    Update dict.ts
    Update: Added language options for Vietnamese writers!
    
    Signed-off-by: vuong2023 <124447749+vuong2023@users.noreply.github.com>

--- src/i18n/dict.ts
+++ src/i18n/dict.ts
@@ -2,10 +2,12 @@ import { Flatten } from '@solid-primitives/i18n'
 import { LANG } from '~/constants'
 import en from './en'
 import zh from './zh'
+import vi from './vi'
 
 export type Dict = Flatten<typeof en>
 
 export default {
   [LANG.EN]: en,
   [LANG.ZH]: zh,
+  [LANG.VI]: vi,
 }
