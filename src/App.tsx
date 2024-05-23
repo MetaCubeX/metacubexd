7172b94bbb625a99821fa41dec96fdff6bdd8de3
0xffffharry <95022881+0xffffharry@users.noreply.github.com>
2024-05-23 08:14:15 +0000

    chore: auto configure from url param

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { usePrefersDark } from '@solid-primitives/media'
 import type { ParentComponent } from 'solid-js'
 import { twMerge } from 'tailwind-merge'
+import { v4 as uuid } from 'uuid'
 import { Header } from '~/components'
 import {
   WsMsg,
@@ -13,6 +14,9 @@ import {
   setLatestConnectionMsg,
   useTwemoji,
   useWsRequest,
+  //
+  setEndpointList,
+  setSelectedEndpoint,
 } from '~/signals'
 
 const ProtectedResources = () => {
@@ -25,6 +29,27 @@ const ProtectedResources = () => {
 
 export const App: ParentComponent = ({ children }) => {
   const prefersDark = usePrefersDark()
+
+  //
+  try {
+    const [searchParams, _] = useSearchParams();
+    const url = searchParams.url
+    if (url) {
+      const u = new URL(decodeURIComponent(url))
+      const apiSecret = u.username
+      u.username = ''
+      const apiURL = u.toString()
+
+      const id = uuid()
+      setEndpointList([{ id, url: apiURL, secret: apiSecret }])
+      setSelectedEndpoint(id)
+      const navigate = useNavigate()
+      navigate('/overview')
+
+      console.log('config from URL: url', apiURL, ', secret: ', apiSecret)
+    }
+  } catch(_) {}
+  //
 
   createEffect(() => {
     if (autoSwitchTheme())
