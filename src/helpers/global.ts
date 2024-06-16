f0286b2d8b8934192a442cd0e60d2828a2b3458d
Xi Gou <gouxivae@gmail.com>
2024-06-16 08:14:16 +0800

    fix: don't allow invalid url like 'http:127.0.0.1'

--- src/helpers/global.ts
+++ src/helpers/global.ts
@@ -3,7 +3,7 @@ export const isSingBox = (version: string) => {
 }
 
 export const transformEndpointURL = (url: string) =>
-  /^https?/.test(url) ? url : `${window.location.protocol}//${url}`
+  /^https?:\/\//.test(url) ? url : `${window.location.protocol}//${url}`
 
 export const useStringBooleanMap = () => {
   const [map, setMap] = createSignal<Record<string, boolean>>({})
