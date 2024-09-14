246a1ac053ef56a1930d3ad2ae3bd0784e01c513
YetAnotherZephyruso <176294927+YetAnotherZephyruso@users.noreply.github.com>
2024-09-14 23:53:54 +0800

    feat: reconnect ws when logs level changed (#1003)

--- src/signals/logs.ts
+++ src/signals/logs.ts
@@ -3,21 +3,28 @@ import { logLevel, logMaxRows } from '~/signals/config'
 import { Log, LogWithSeq } from '~/types'
 
 let seq = 1
-const [logs, setLogs] = createSignal<LogWithSeq[]>([])
 
-const logsData = useWsRequest<Log>('logs', { level: logLevel() })
+const [logs, setLogs] = createSignal<LogWithSeq[]>([])
 const [paused, setPaused] = createSignal(false)
 
-createEffect(() => {
-  const data = logsData()
+createEffect(
+  on(logLevel, (value, oldValue) => {
+    if (value === oldValue) return
 
-  if (!data || paused()) {
-    return
-  }
+    const logsData = useWsRequest<Log>('logs', { level: logLevel() })
 
-  setLogs((logs) => [{ ...data, seq }, ...logs].slice(0, logMaxRows()))
-  seq++
-})
+    createEffect(() => {
+      const data = logsData()
+
+      if (!data || paused()) {
+        return
+      }
+
+      setLogs((logs) => [{ ...data, seq }, ...logs].slice(0, logMaxRows()))
+      seq++
+    })
+  }),
+)
 
 export const useLogs = () => {
   return {
