e6fa749a28e55b9ea85a5ccae71346823fd2567c
kunish <kunish.butt@gmail.com>
2024-10-13 12:03:06 +0800

    fix(i18n): time locale not working

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,4 +1,5 @@
-import dayjs, { locale } from 'dayjs'
+import dayjs from 'dayjs'
+import { locale } from '~/i18n'
 
 export const formatTimeFromNow = (time: number | string) =>
   dayjs(time).locale(locale()).fromNow()
