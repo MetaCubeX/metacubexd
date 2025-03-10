d82743fa7d5b7fbdf4c9046054f4e26bd582b457
kunish <kunish@users.noreply.github.com>
2025-03-10 03:53:57 +0000

    v1.177.1 4a0b74062a339905819054f5e9fff6b3cbec4f4a

---
+++ assets/DocumentTitle-DWzSOlPW.js
@@ -0,0 +1,1 @@
+import{d as t,bB as r}from"./index-CmewMs7z.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
---
+++ assets/time-D9GSIcux.js
@@ -0,0 +1,1 @@
+import{V as a,b3 as m}from"./index-CmewMs7z.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
