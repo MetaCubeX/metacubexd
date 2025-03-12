af8b3e2f7003e9831aa3c8a941f5bd5bb8fefc41
kunish <kunish.butt@gmail.com>
2025-03-12 11:18:31 +0800

    feat: add custom font theme variables for Twemoji and system fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,4 @@ import { Config } from 'tailwindcss'
 export default {
   content: ['src/**/*.{css,ts,tsx}'],
   plugins: [corvu],
-  theme: {
-    extend: {
-      fontFamily: {
-        twemoji: ['system-ui', 'Twemoji Mozilla', 'Fira Sans', 'monospace'],
-        'no-twemoji': ['system-ui', 'Fira Sans', 'monospace'],
-      },
-    },
-  },
 } as Config
