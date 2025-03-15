8c5d1845a7f5e780014f2f8288f6e5602687225a
kunish <kunish.butt@gmail.com>
2025-03-15 20:58:40 +0800

    feat(proxy-node): update ProxyNodeCard styling and structure for improved layout and clarity

--- src/components/ProxyNodeCard.tsx
+++ src/components/ProxyNodeCard.tsx
@@ -59,29 +59,34 @@ export const ProxyNodeCard = (props: {
       }}
     >
       <Tooltip.Anchor
+        as="div"
         class={twMerge(
-          'card bg-neutral text-neutral-content',
-          isSelected &&
-            'bg-gradient-to-br from-primary to-secondary text-primary-content',
-          onClick && 'cursor-pointer',
+          'indicator card w-full bg-neutral text-neutral-content',
+          isSelected && 'bg-primary text-primary-content',
         )}
         title={title()}
       >
+        <Show when={isUDP()}>
+          <div class="indicator-item badge badge-xs badge-info">U</div>
+        </Show>
+
         <Tooltip.Trigger>
-          <div class="card-body gap-1 space-y-1 p-2.5" onClick={onClick}>
+          <div
+            class={twMerge(
+              'card-body gap-1 space-y-1 p-2.5',
+              onClick && 'cursor-pointer',
+            )}
+            onClick={onClick}
+          >
             <h2 class="card-title line-clamp-1 text-start text-sm break-all">
               {proxyName}
             </h2>
 
-            <div class="card-actions items-center justify-between gap-1">
-              <div class="flex items-center gap-1">
+            <div class="card-actions items-end justify-between gap-1">
+              <div class="flex flex-col gap-0.5">
                 <div class="text-xs font-semibold uppercase opacity-75">
                   {formatProxyType(proxyNode()?.type)}
                 </div>
-
-                <Show when={isUDP()}>
-                  <div class="badge badge-ghost badge-xs">U</div>
-                </Show>
               </div>
 
               <Latency
@@ -109,7 +114,7 @@ export const ProxyNodeCard = (props: {
           <Tooltip.Content class="z-50">
             <Tooltip.Arrow class="text-neutral" />
 
-            <div class="flex flex-col items-center gap-2 rounded-box bg-neutral bg-gradient-to-br from-primary to-secondary p-2.5 text-primary-content shadow-lg">
+            <div class="flex flex-col items-center gap-2 rounded-box bg-primary p-2.5 text-primary-content shadow-lg">
               <h2 class="text-lg font-bold">{proxyName}</h2>
 
               <Show when={specialTypes()}>
