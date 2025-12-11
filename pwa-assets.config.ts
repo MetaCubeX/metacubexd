3408a9f820a4a8e4d910dd6250e4049b5b0f1dfd
kunish <kunish.butt@gmail.com>
2025-12-11 13:20:02 +0800

    chore: remove unused PWA assets configuration file

--- pwa-assets.config.ts
+++
@@ -1,9 +0,0 @@
-import {
-  defineConfig,
-  minimal2023Preset,
-} from '@vite-pwa/assets-generator/config'
-
-export default defineConfig({
-  preset: minimal2023Preset,
-  images: ['public/favicon.svg'],
-})
